080ab349f084b2c39c5c6606eb21371d389ff4ab
Simon-Pierre Rhéaume <sprheaume@gmail.com>
2020-04-02 07:39:07 -0300

    final version submitted to fCC

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+//
+//
+// File Metadata for freeCodeCamp
+// by Simon Rhe, April 2020
+//
+//
+
+
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer');
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
+const upload = multer(); // options object {dest: 'uploads/'} ommitted so that files are not written to disk
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +24,17 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+// User story: When I submit something, I will receive the file
+// name, and size in bytes within the JSON response.
+// example: {"name":"testfile.txt","type":"text/plain","size":31}
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
